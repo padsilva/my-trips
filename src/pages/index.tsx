c6f5be458364eec320d4b9a2dc2968f9b6f96076
paulosilva <paulosilva@chinasystems.com>
2021-03-05 19:06:54 +0000

    page and component about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import dynamic from 'next/dynamic'
 import places from 'components/Map/mock'
+import LinkWrapper from 'components/LinkWrapper'
+import { InfoOutline } from '@styled-icons/evaicons-outline'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
-const Home = () => <Map places={places} />
+const Home = () => (
+  <>
+    <LinkWrapper href="/about">
+      <InfoOutline size={32} aria-label="About" />
+    </LinkWrapper>
+    <Map places={places} />
+  </>
+)
 
 export default Home
