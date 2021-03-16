ff77128bf09ed525cce7d632d377f52ae93f5700
paulosilva <paulosilva@chinasystems.com>
2021-03-16 13:16:58 +0000

    add sitemap

---
+++ src/pages/sitemap.xml/index.tsx
@@ -0,0 +1,33 @@
+import { getServerSideSitemap } from 'next-sitemap'
+import { GetServerSideProps } from 'next'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
+import { GET_PLACES } from 'graphql/queries'
+import client from 'graphql/client'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // Method to source urls from cms
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
+
+  const fields = places.map(({ slug }) => ({
+    loc: `https://padsilva-trips.vercel.app/${slug}`, // Absolute url
+    lastmod: new Date().toISOString()
+  }))
+
+  fields.push(
+    {
+      loc: 'https://padsilva-trips.vercel.app/',
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: 'https://padsilva-trips.vercel.app/about',
+      lastmod: new Date().toISOString()
+    }
+  )
+
+  return getServerSideSitemap(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const Named = () => {}
+export default Named
